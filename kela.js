51bf561ad495652dd778c799a1316e44961d5dd9
Mahammad Ashif <mahdashif6@gmail.com>
2026-09-15 16:40:57 +0530

    initial

--- kela.js
+++ kela.js
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Button Example</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <button id="myButton" class="btn">Click Me!</button>
-
-  <script src="baklol.js"></script>
-</body>
-</html>
+score=90
+if score >=90:
+    print("Grade:A")
+elif score >=80:
+    print("Grade:B")
+else:
+    print("Grade:C")
