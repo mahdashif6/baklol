e9461106984f33c8c2da3496837ddd5f695f7f9e
Mahammad Ashif <mahdashif6@gmail.com>
2026-09-22 17:05:14 +0530

    Initial commit

---
+++ prgrm.js
@@ -0,0 +1,19 @@
+let studentName="MAHD";
+let age=18;
+var course="Data Science";
+const collegeName="SITM";
+let marks=80;
+const Birthyear=2008;
+let city="Barpeta";
+let semester="First";
+let favouritesubject="javascript";
+
+console.log("Student Name:",studentName);
+console.log("age:",age);
+console.log("course:",course);
+console.log("college name:",collegeName);
+console.log("marks:",marks);
+console.log("birthyear:",Birthyear);
+console.log("city:",city);
+console.log("semester:",semester);
+console.log("favouritesubject:",favouritesubject);
